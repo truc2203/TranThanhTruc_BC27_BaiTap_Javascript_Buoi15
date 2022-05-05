4f0237c8bf78e67b7527d2169b329f5fa1dc285c
truc2203 <tranthanhtruc22031998@gmail.com>
2022-05-05 12:13:56 +0700

    BT1

--- Bt1.js
+++ Bt1.js
@@ -11,43 +11,54 @@ function studentManagement()
 
     var result = document.getElementById("result")
 
+    //Các biến hứng giá trị của hàm tổng điểm và tổng điểm ưu tiên
     var totalPoints = totalPoint(firstSub,secondSub,thirdSub)
-    var totalArea = area(priArea)
-    var totalObj = object(priObj)
-    
-    var finalPoint = totalPoints + totalArea + totalObj
-    if(targetPoint <= finalPoint && totalPoint !== 0)
+    var totalPriority = priority(priArea,priObj)
+
+    var check = checkPoint(firstSub,secondSub,thirdSub)
+
+    //Điểm tổng kết = tổng điểm + tổng điểm ưu tiên
+    var finalPoint = totalPoints + totalPriority
+
+    //Đạt nếu điểm chuẩn < điểm tổng kết và không có môn nào 0đ
+    if(targetPoint <= finalPoint && check === 1)
     {
-        result.innerHTML = "Pass"
-        console.log(finalPoint)
+        result.innerHTML = ` Bạn đã đậu với tổng điểm : ${finalPoint}đ`
     }
+    //Không đạt
     else{
-        result.innerHTML = "Fail"
-        console.log(finalPoint)
+        result.innerHTML = ` Bạn đã rớt với tổng điểm : ${finalPoint}đ`
     }
 }
 
-
-
+//Hàm tính tổng điểm 
 function totalPoint(firstSub, secondSub,thirdSub)
 {
-    var total
-    if(firstSub > 0 && secondSub > 0 && thirdSub > 0)
-    {
-        total = firstSub + secondSub + thirdSub
-        return total
-    }
-    else{
-        return 0
-    }
+    var total = firstSub + secondSub + thirdSub
+    return total
 }
 
 
+// Hàm kiểm tra rớt nếu có 1 trong 3 môn 0đ
+function checkPoint(firstSub,secondSub,thirdSub)
+{
+    var check = 0
+    if(firstSub > 0 && secondSub > 0 && thirdSub > 0)
+    {
+        check += 1
+        return check
+    }
+    else{
+        return check
+    }
+}
 
-
-function area(priArea)
+// Hàm tính tổng điểm ưu tiên theo khu vực + đối tượng
+function priority(priArea,priObj)
 {
     var areaPoint = 0
+    var objPoint = 0
+    var priTotal = 0
     if(priArea === "A")
     {
         areaPoint += 2
@@ -64,13 +75,6 @@ function area(priArea)
     {
         areaPoint = 0
     }
-    return areaPoint
-
-}
-
-function object(priObj)
-{
-    var objPoint = 0
 
     if(priObj === "1")
     {
@@ -88,5 +92,7 @@ function object(priObj)
     {
         objPoint = 0
     }
-    return objPoint
+    return priTotal = areaPoint + objPoint
+
 }
+
