c9898830cfdded2bee9e3a19fadab7d87dbae4f3
truc2203 <tranthanhtruc22031998@gmail.com>
2022-05-06 12:20:23 +0700

    BT2

---
+++ BT2_ElectricBill.js
@@ -0,0 +1,39 @@
+function electricBill()
+{
+    var userName = document.getElementById("userName").value 
+    var kwNumber = +document.getElementById("kwNumber").value
+    var customer = document.getElementById("customer")
+    var paying = document.getElementById("paying")
+
+    var totalPay = calcKW(kwNumber,500,650,850,1100,1300).toLocaleString()
+
+    customer.innerHTML = `Tên khách hàng : ${userName}`
+    paying.innerHTML = `Tổng tiền : ${totalPay}đ`
+
+}
+
+function calcKW(kwNumber,kw_1_50,kw_50_100,kw_100_200,kw_200_350, kws)
+{
+    var total = 0
+    if(kwNumber <= 50)
+    {
+        total = kwNumber * kw_1_50
+    }
+    else if(kwNumber > 50 && kwNumber <= 100)
+    {
+        total = 50*kw_1_50 + (kwNumber - 50)*kw_50_100
+    }
+    else if(kwNumber > 100 && kwNumber <= 200)
+    {
+        total = 50*kw_1_50 + 50*kw_50_100 + (kwNumber - 100)*kw_100_200
+    }
+    else if(kwNumber > 200 && kwNumber <= 350)
+    {
+        total = 50*kw_1_50 + 50*kw_50_100 + 100*kw_100_200 + (kwNumber - 200)*kw_200_350
+    }
+    else 
+    {
+        total = 50*kw_1_50 + 50*kw_50_100 + 100*kw_100_200 + 150*kw_200_350 + (kwNumber - 350)*kws
+    }
+    return total
+}
